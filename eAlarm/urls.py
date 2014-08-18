8939e62a26d124d9a32c0e8250a908175ba76890
diennd <ducdienpt@gmail.com>
2014-08-18 17:00:57 +0700

    d

--- eAlarm/urls.py
+++ eAlarm/urls.py
@@ -2,18 +2,19 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 from eAlarm import settings
-from myapp.views import Home,DeviceProperties
+from myapp.views import Home,DeviceProperties,AddDeviceProperties,EditDeviceProperties
 
 
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^home$', Home.index),
-    url(r'^device-property$', DeviceProperties.index),
     url(r'^$', Home.index),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name': 'signin.html'}, name='login'),
-    url(r'^logout$', 'django.contrib.auth.views.logout',
-                          {'next_page': 'login'}),
+    url(r'^logout$', 'django.contrib.auth.views.logout', {'next_page': 'login'}),
+    url(r'^device-property$', DeviceProperties.index),
+    url(r'^add-device-property$', AddDeviceProperties.index,name='add-device-property'),
+    url(r'^edit-device-property$', EditDeviceProperties.index,name='add-device-property'),
     url(regex=r'^(?P<path>.*)$', view='django.views.static.serve', kwargs={'document_root': settings.STATIC_ROOT, 'show_indexes' : False, }),
 )
--- myapp/views/DeviceProperties.py
+++ myapp/views/DeviceProperties.py
@@ -4,6 +4,7 @@ Created on Apr 3, 2014
 @author: TuanNA
 '''
 # @login_required(login_url='/signin')
+from django.core.context_processors import csrf
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
 
@@ -14,4 +15,5 @@ from myapp.models import DeviceProperties
 def index(request):
 	lsDeviceProperty = DeviceProperties.objects.all()
 	context={'lsDeviceProperty':lsDeviceProperty}
+	context.update(csrf(request))
 	return render_to_response("device-property.html", context)
