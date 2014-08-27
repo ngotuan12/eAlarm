e77761ee2271d3a6e2fa8f9a4b37b41704e7cca7
diennd <ducdienpt@gmail.com>
2014-08-27 17:11:11 +0700

    d

--- myapp/views/Device.py
+++ myapp/views/Device.py
@@ -4,14 +4,16 @@ Created on Apr 3, 2014
 @author: TuanNA
 '''
 # @login_required(login_url='/signin')
+from django.contrib.auth.decorators import login_required, permission_required
 from django.core.context_processors import csrf
-from django.contrib.auth.decorators import login_required
 from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
-from myapp.models import Area,Device
+from myapp.models import Area, Device
 
 
 @login_required(login_url='/login')
+@permission_required('myapp.view_device',login_url='/permission-error')
 def index(request):
 	try:
 		lsArea = Area.objects.filter(level = '2')
@@ -22,4 +24,4 @@ def index(request):
 		context={}
 		print(ex)
 	finally:
-		return render_to_response("device.html", context)
+		return render_to_response("device.html", context,RequestContext(request))
