ff8716c1b99db4c05b6e2a323eb4887a3799bc02
diennd <ducdienpt@gmail.com>
2014-12-04 14:00:17 +0700

    d

--- eAlarmWebapp/myapp/views/ListDevice.py
+++ eAlarmWebapp/myapp/views/ListDevice.py
@@ -14,6 +14,6 @@ from myapp.models import Device
 @login_required(login_url='/login')
 @permission_required('myapp.view_device',login_url='/permission-error')
 def index(request):
-	devices = Device.objects.all()
+	devices = Device.objects.filter(type='1')
 	context={'devices':devices}
 	return render_to_response("list-device.html", context, RequestContext(request))
